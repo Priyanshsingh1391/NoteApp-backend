97ee582c7cb6dcda5a7c7e1eae3bb729354830eb
priyanshsingh <singh.priyansh1391@gmail.com>
2025-08-19 17:53:45 +0530

    new

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const connectToMongoDB = require('./db');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://note-app-frontend1.vercel.app/",
+    credentials: true
+}
+));
 app.use(express.json());
 app.use('/api/auth', authRouter);
 app.use('/api/note', noteRouter )
